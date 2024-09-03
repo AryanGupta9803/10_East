3785992d64dfe225fd00b1f9fd0b561ad84395e7
Aryan <09aryangupta@gmail.com>
2024-09-03 19:02:31 +0800

    environment setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// frontend/tailwind.config.js
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
